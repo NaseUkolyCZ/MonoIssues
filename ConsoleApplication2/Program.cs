2555d69dc9309fd125be2c9b4ec542623e908b0e
davidpodhola <david.podhola@naseukoly.cz>
2014-04-14 14:13:24 +0200

    initial commit

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Parse("2014-04-16+02:00") );
+            Console.ReadLine();
+        }
+    }
+}
